c73c7af1b5c0d12f9977e7f1995acdb2cce0319a
Hack Reactor Students <uknoiluvu@gmail.com>
2014-02-27 22:32:12 -0800

    linkedList 20140227

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(list.head === null && list.tail === null){
+      list.head = node;
+      list.tail = node;
+    }else{
+      var oldTail = list.tail;
+      oldTail.next = node;
+      list.tail = node;
+
+      // list.head.next = node;
+      // list.tail = node;
+
+    }
+
   };
 
   list.removeHead = function(){
+    var oldHead = list.head;
+    list.head = oldHead.next;
   };
 
   list.contains = function(target, node){
+    console.log(target, node);
   };
 
   return list;
@@ -22,3 +39,21 @@ var makeNode = function(value){
 
   return node;
 };
+//    if(list.head === null){
+//
+    /*
+      list.head = null;
+      list.head = list.tail || null;
+      list.node.next = list.tail;
+      list.tail = makeNode(value);
+    */
+/*
+      }else{
+      list.tail.next = value;
+      list.tail = makeNode(value);
+    }
+*/
+/*
+    list.head = list.head.next;
+    console.log(list);
+*/
