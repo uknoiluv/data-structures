782f31949b69564806845b0a982be0441ee537be
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-01 12:25:09 -0800

    Finished Binary Search Tree Extra Credit B^)

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,71 @@
 var makeBinarySearchTree = function(value){
+  var newTree = {};
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  _.extend(newTree, treeMethods);
+  return newTree;
+};
+
+var treeMethods = {
+  "insert": function(value){
+    var newTree = makeBinarySearchTree(value);
+    var tryInsert = function(target){
+      if(value < target.value){
+        if(target.left === null){
+          target.left = newTree;
+        }
+        else {
+          tryInsert(target.left);
+        }
+      }
+      else if(value > target.value){
+        if(target.right === null){
+          target.right = newTree;
+        }
+        else{
+          tryInsert(target.right);
+        }
+      }
+    };
+    tryInsert(this);
+  },
+
+  "contains": function(value){
+    var result = false;
+    var findValue = function(target){
+      if(value < target.value){
+        if(target.left === null){
+        }
+        else {
+          findValue(target.left);
+        }
+      }
+      else if(value > target.value){
+        if(target.right === null){
+        }
+        else{
+          findValue(target.right);
+        }
+      }
+      else if (value === target.value) {
+        result = true;
+      }
+    };
+    findValue(this);
+    return result;
+  },
+
+  "depthFirstLog": function(iterator){
+    traverseTree = function(node){
+      iterator(node);
+      if(node.left !== null){
+        traverseTree(node.left);
+      }
+      if(node.right !== null){
+        traverseTree(node.right);
+      }
+    };
+    traverseTree(this);
+  }
 };
