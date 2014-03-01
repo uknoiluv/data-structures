0475183a0ce41b23cb9850195a79f5f1127eb3e1
Hack Reactor Students <uknoiluvu@gmail.com>
2014-02-28 19:39:16 -0800

    Finished hash table for the third time \o/

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,59 +3,33 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-
 HashTable.prototype.insert = function(k, v){
-  // var i = getIndexBelowMaxForKey(k, this._limit);
-  var generateIndex = function(key, limit) {
-    var keyCode = 0;
-    _.each(key, function(letter){
-      keyCode = keyCode + letter.charCodeAt();
-    });
-    keyCode = keyCode % limit;
-  };
-  var i = generateIndex(k, this._limit);
-  if (!Array.isArray(this._storage[i])) {
+  var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if(!Array.isArray(this._storage[i])){
     this._storage[i] = [];
   }
-  this._storage[i].push([k, v]);
+  this._storage[i].push([k,v]);
+
 };
 
 HashTable.prototype.retrieve = function(k){
-  // var i = getIndexBelowMaxForKey(k, this._limit);
-  var generateIndex = function(key, limit) {
-    var keyCode = 0;
-    _.each(key, function(letter){
-      keyCode = keyCode + letter.charCodeAt();
-    });
-    keyCode = keyCode % limit;
-  };
-  var i = generateIndex(k, this._limit);
-  var storage = this._storage;
-  var item;
-  _.each(storage[i], function(value){
-    if(value[0] === k){
-      item = value[1];
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var found = undefined;
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      found = this._storage[i][j][1];
     }
-  });
-  return item;
+  }
+  return found;
 };
 
 HashTable.prototype.remove = function(k){
-  var generateIndex = function(key, limit) {
-    var keyCode = 0;
-    _.each(key, function(letter){
-      keyCode = keyCode + letter.charCodeAt();
-    });
-    keyCode = keyCode % limit;
-  };
-  var i = generateIndex(k, this._limit);
-  var storage = this._storage;
-  _.each(storage[i], function(value){
-    if(value[0] === k){
-      delete(value);
-      console.log(value);
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      this._storage[i][j][1] = null;
     }
-  });
+  }
 };
-
 
